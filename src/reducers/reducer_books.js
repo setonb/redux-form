e29d46f779c95b36fb80ef7add0ceaab576aee98
Seton Brown <seton_brown@ziffdavis.com>
2017-06-30 15:38:53 -0400

    Starting Redux tutorial

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Tuesday\'s With Morrie'},
+    {title: 'The Man Who Would Be King'},
+    {title: 'Harry Potter'},
+    {title: 'The Davinci Code'}
+  ]
+}
